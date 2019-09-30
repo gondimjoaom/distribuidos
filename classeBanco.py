beeb72acc72641dbb3e83d9042e3a66f22096ad2
Paula Tavares <paulatavaresfranca@gmail.com>
2019-09-29 22:03:29 -0300

    add new logic

--- classeBanco.py
+++ classeBanco.py
@@ -2,81 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from classeCliente import Client
-import socket
-
-def verSaldo(nome, doc):
-    for cliente in clientes:
-        if (cliente.nome == nome and cliente.doc == doc):
-            return cliente.verSaldo().encode()
-def deposito(nome, doc, valor):
-    for cliente in clientes:
-        if (cliente.nome == nome and cliente.doc == doc):
-            cliente.saldo += valor
-def saque(nome, doc, valor):
-    for cliente in clientes:
-        if (cliente.nome == nome and cliente.doc == doc):
-            if(cliente.saldo < valor):
-                return 'error'
-            cliente.saldo -= valor
-            return 'success'
-def transferencia(nome, doc, valor, nome_destinatario, doc_destinatario):
-    result = saque(nome, doc, int(valor))
-    if(result == 'success'):
-        deposito(nome_destinatario, doc_destinatario, int(valor))
-        return 'success' 
-    else:
-        return 'error'
-            
-
-def sendString(msg):
-    return conn.send(msg.encode())
-
-cliente1 = Client('Joao', '123', 500)
-cliente2 = Client('Paula', '321', 1000)
-
-clientes = []
-
-clientes.append(cliente1)
-clientes.append(cliente2)
+import socket, threading, json
 
 #estabelecendo conexão
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('127.0.0.7', 8000))
-server.listen(5)
+server.bind(('127.0.0.1', 8000))
 
 print("Banco Gringotes iniciado e escutando na porta 8000")
 
-conn, add = server.accept()
-
-clienteNome = conn.recv(4096).decode()
-clienteDoc = conn.recv(4096).decode()
-print('Cliente {}, de documento {}, se conectou usando o IP {} com nome {}.'.format(clienteNome, clienteDoc,add[0],socket.gethostbyaddr(add[0])[0]))
 
 while True:
-    data = conn.recv(4096).decode()
-    if data == 'saldo':
-        conn.send(verSaldo(clienteNome, clienteDoc))
-    elif data == 'deposito':
-        valor = conn.recv(4096).decode()
-        deposito(clienteNome, clienteDoc, int(valor))
-        conn.send(verSaldo(clienteNome, clienteDoc))
-    elif data == 'saque':
-        valor = conn.recv(4096).decode()
-        result = saque(clienteNome, clienteDoc, int(valor))
-        if(result == 'success'):
-            conn.send(verSaldo(clienteNome, clienteDoc))
-        else:
-            conn.send(str("Não é possível realizar essa operação: saldo insuficiente").encode())
-    elif data == 'transferencia':
-        nome_destinatario = conn.recv(4096).decode()
-        doc_destinatario = conn.recv(4096).decode()
-        valor = conn.recv(4096).decode() 
-        result = transferencia(clienteNome, clienteDoc, int(valor), nome_destinatario, doc_destinatario)
-        if(result == 'success'):
-            conn.send(str("Transferência realizada com sucesso").encode())
-        else:
-            conn.send(str("Não é possível realizar essa operação: saldo insuficiente").encode())          
-    elif not data:
-        break
-
-server.close()
+    server.listen(1)
+    conn, add = server.accept()
+    newCliente = Client(conn, add)
+    newCliente.start()
--- classeCliente.py
+++ classeCliente.py
@@ -1,12 +1,73 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-class Client (object):
-    def __init__(self, nome, doc, saldo = 0):
-        self.nome = nome
-        self.doc = doc
+import socket, threading
+class Client (threading.Thread):
+    def __init__(self, csocket, caddres, saldo = 0):
+        threading.Thread.__init__(self)
+        self.clientSocket = csocket
+        self.clienteAddress = caddres
+        self.nome = self.clientSocket.recv(4096).decode()
+        self.doc = self.clientSocket.recv(4096).decode()
         self.saldo = saldo
+        
+
+    def run(self):
+        print('Cliente {} conectado através do IP {}.'.format(self.nome, self.clienteAddress[0]))
+        while True:
+            msg = self.clientSocket.recv(4096).decode()
+            if msg == 'xau' or not msg:
+                break
+            elif msg == 'saldo':
+                print('Cliente {} solicitou visualizar o seu saldo.'.format(self.nome))
+                self.clientSocket.send(self.verSaldo().encode())
+            elif msg == 'deposito':
+                print('Cliente {} deseja fazer um deposito. Aguardando o valor'. format(self.nome))
+                valor = self.clientSocket.recv(4096).decode()
+                self.deposito(int(valor))
+                print('Valor de {} para deposito na conta de {}.'.format(valor, self.nome))
+                self.clientSocket.send(self.verSaldo().encode())
+            elif msg == 'saque':
+                print('Cliente {} deseja fazer um saque. Aguardando o valor'. format(self.nome))                
+                valor = self.clientSocket.recv(4096).decode()
+                result = self.saque(int(valor))
+                if(result == 'success'):
+                    self.clientSocket.send(self.verSaldo().encode())
+                    print('Valor de {} para saque na conta de {}.'.format(valor, self.nome))                    
+                else:
+                    self.clientSocket.send(str("Não é possível realizar essa operação: saldo insuficiente").encode())
+                    print('Error - Valor de {} para deposito na conta de {}.'.format(valor, self.nome))
+            elif msg == 'transferencia':
+                nome_destinatario = self.clientSocket.recv(4096).decode()
+                doc_destinatario = self.clientSocket.recv(4096).decode()
+                valor = self.clientSocket.recv(4096).decode() 
+                result = self.transferencia(int(valor), nome_destinatario, doc_destinatario)
+                if(result == 'success'):
+                    self.clientSocket.send(str("Transferência realizada com sucesso").encode())
+                else:
+                    self.clientSocket.send(str("Não é possível realizar essa operação: saldo insuficiente").encode())
+                print(msg)
+            else:
+                print(msg)
+        print('{} desconectou.'.format(self.nome)) 
     
-    def verSaldo (self):
-        saldo = "Seu saldo é de R$" + str(self.saldo) + "."
+    def verSaldo(self):
+        saldo = "\nSeu saldo é de R$" + str(self.saldo) + "."
         return saldo
+        
+    def deposito(self, valor):
+        self.saldo += int(valor)
+            
+    def saque(self, valor):
+        if(self.saldo < valor):
+            return 'error'
+        self.saldo -= valor
+        return 'success'
+                
+    def transferencia(self, valor, nome_destinatario, doc_destinatario):
+        result = self.saque(valor)
+        if(result == 'success'):
+            # Implementar deposito para transferência
+            return 'success' 
+        else:
+            return 'error'
