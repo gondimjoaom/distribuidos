bb03811ee59d2ec40c49a1ce1232e4c108ec8ead
Paula Tavares <paulatavaresfranca@gmail.com>
2019-09-30 23:20:18 -0300

    removing switch case

--- classeCliente.py
+++ classeCliente.py
@@ -18,42 +18,39 @@ class Client (threading.Thread):
         print('Cliente {} de documento {} conectado através do IP {}.'.format(self.nome, self.doc, self.clienteAddress[0]))
         while True:
             msg = self.clientSocket.recv(4096).decode()
-            #
-            # Switch case para determinar qual operação está sendo executada
-            #
-            switch(msg):
-                case 'saldo':
-                    print('Cliente {} solicitou visualizar o seu saldo.'.format(self.nome))
-                    self.clientSocket.send(self.verSaldo().encode())
-                case 'deposito':
-                    print('Cliente {} deseja fazer um deposito. Aguardando o valor'. format(self.nome))
-                    valor = self.clientSocket.recv(4096).decode()
-                    self.deposito(int(valor))
-                    print('Valor de {} para deposito na conta de {}.'.format(valor, self.nome))
-                    self.clientSocket.send(self.verSaldo().encode())
-                case 'saque':
-                    print('Cliente {} deseja fazer um saque. Aguardando o valor'. format(self.nome))                
-                    valor = self.clientSocket.recv(4096).decode()
-                    result = self.saque(int(valor))
-                    if(result == 'success'):
-                        self.clientSocket.send('Saque realizado com sucesso. {}'.format(self.verSaldo()).encode())
-                        print('Valor de {} para saque na conta de {}.'.format(valor, self.nome))
-                    else:
-                        self.clientSocket.send(str("Não é possível realizar essa operação: saldo insuficiente").encode())
-                        print('Error - Valor de {} para deposito na conta de {}.'.format(valor, self.nome))
-                case 'transferencia':
-                    nome_destinatario = self.clientSocket.recv(4096).decode()
-                    doc_destinatario = self.clientSocket.recv(4096).decode()
-                    valor = self.clientSocket.recv(4096).decode() 
-                    result = self.transferencia(int(valor), nome_destinatario)
-                    if(result == 'success'):
-                        self.clientSocket.send(str("Transferência realizada com sucesso").encode())
-                    else:
-                        self.clientSocket.send(str("Não é possível realizar essa operação: saldo insuficiente").encode())
-                    print(msg)
-                default:
-                    print(msg)
-                    break                    
+            switch(msg) == 'xau' or not msg:
+                break
+            elif msg == 'saldo':
+                print('Cliente {} solicitou visualizar o seu saldo.'.format(self.nome))
+                self.clientSocket.send(self.verSaldo().encode())
+            elif msg == 'deposito':
+                print('Cliente {} deseja fazer um deposito. Aguardando o valor'. format(self.nome))
+                valor = self.clientSocket.recv(4096).decode()
+                self.deposito(int(valor))
+                print('Valor de {} para deposito na conta de {}.'.format(valor, self.nome))
+                self.clientSocket.send(self.verSaldo().encode())
+            elif msg == 'saque':
+                print('Cliente {} deseja fazer um saque. Aguardando o valor'. format(self.nome))                
+                valor = self.clientSocket.recv(4096).decode()
+                result = self.saque(int(valor))
+                if(result == 'success'):
+                    self.clientSocket.send('Saque realizado com sucesso. {}'.format(self.verSaldo()).encode())
+                    print('Valor de {} para saque na conta de {}.'.format(valor, self.nome))
+                else:
+                    self.clientSocket.send(str("Não é possível realizar essa operação: saldo insuficiente").encode())
+                    print('Error - Valor de {} para deposito na conta de {}.'.format(valor, self.nome))
+            elif msg == 'transferencia':
+                nome_destinatario = self.clientSocket.recv(4096).decode()
+                doc_destinatario = self.clientSocket.recv(4096).decode()
+                valor = self.clientSocket.recv(4096).decode() 
+                result = self.transferencia(int(valor), nome_destinatario)
+                if(result == 'success'):
+                    self.clientSocket.send(str("Transferência realizada com sucesso").encode())
+                else:
+                    self.clientSocket.send(str("Não é possível realizar essa operação: saldo insuficiente").encode())
+                print(msg)
+            else:
+                print(msg)
         print('{} desconectou.'.format(self.nome)) 
     
     def verSaldo(self):
